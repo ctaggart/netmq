aad06d0c5e874a4e49cfb8d69168f6530e74bbe9
Cameron Taggart <cameron.taggart@gmail.com>
2018-01-09 17:20:37 -0500

    works for mono too when compiled for net462

--- docker/build.ps1
+++ docker/build.ps1
@@ -1,4 +1,5 @@
 cd $PSScriptRoot
 msbuild /t:restore /v:m
-# msbuild /t:build /v:m
-msbuild /t:publish /v:m
+# msbuild /t:publish /v:m /p:TargetFramework=net462 /p:RuntimeIdentifier=win-x64
+# msbuild /t:publish /v:m /p:TargetFramework=net462
+msbuild /t:build /v:m
